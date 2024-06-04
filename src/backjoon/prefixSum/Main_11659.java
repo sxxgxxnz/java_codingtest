785eb8f3763aff312abadafff56dba18f4013d03
sxxgxxnz <tmddms2292@gmail.com>
2024-06-04 19:15:12 +0900

    240604

---
+++ src/backjoon/prefixSum/Main_11659.java
@@ -0,0 +1,34 @@
+package backjoon.prefixSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        arr[0] = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++){
+            // 누적 합 저장
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) ;
+            int end = Integer.parseInt(st.nextToken()) ;
+
+            sb.append(arr[end] - arr[start - 1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
